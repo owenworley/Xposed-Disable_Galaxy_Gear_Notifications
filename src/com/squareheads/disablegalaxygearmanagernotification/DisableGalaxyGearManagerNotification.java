de8f3dcba41b324974f21c465a2856918d98de53
Owen Worley <owen@blinkbox.com>
2014-04-14 10:54:46 +0100

    Added additional hooking mechanism for new verions of gear manager. Samsung has split off watch operations for the original Gear to a plugin, so we are hooking this now. Also wrapped hook attempts in try catch to not pollute the Xposed debug log

--- src/com/squareheads/disablegalaxygearmanagernotification/DisableGalaxyGearManagerNotification.java
+++ src/com/squareheads/disablegalaxygearmanagernotification/DisableGalaxyGearManagerNotification.java
@@ -2,6 +2,7 @@ package com.squareheads.disablegalaxygearmanagernotification;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedHelpers;
 
@@ -10,15 +11,37 @@ public class DisableGalaxyGearManagerNotification implements IXposedHookLoadPack
 
 	@Override
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-		if (!lpparam.packageName.equals("com.samsung.android.app.watchmanager"))
-			return;
+		if (lpparam.packageName.equals("com.samsung.android.app.watchmanager")){
+			
+			try {
+				XposedHelpers.findAndHookMethod(
+						"com.samsung.android.app.watchmanager.service.BManagerConnectionService",
+						lpparam.classLoader,
+						"setNotification",
+						XC_MethodReplacement.DO_NOTHING
+						);
+			} catch (Exception e) {
+				//Do nothing.
+			}
+		}
+		else if (lpparam.packageName.equals("com.samsung.android.gear1plugin")){
+			try {
+				XposedHelpers.findAndHookMethod(
+						"com.samsung.android.gear1plugin.service.BManagerConnectionService",
+						lpparam.classLoader,
+						"setNotification",
+						XC_MethodReplacement.DO_NOTHING
+						);
+			} catch (Exception e) {
+				//Do nothing
+			}
+		}
+		return;
 		
-		XposedHelpers.findAndHookMethod(
-				"com.samsung.android.app.watchmanager.service.BManagerConnectionService",
-				lpparam.classLoader,
-				"setNotification",
-				XC_MethodReplacement.DO_NOTHING
-				);
+		
+		
+		
+		
 	}
 }
 
