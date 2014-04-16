023c99daf01940c2a9feac71dbe5943e164e1dd3
Owen Worley <owen@blinkbox.com>
2014-04-16 12:00:46 +0100

    Removed hooks for older versions of gear manager. Now only supports latest gear manager, as of writing this is 2.0.14040401

--- src/com/squareheads/disablegalaxygearmanagernotification/DisableGalaxyGearManagerNotification.java
+++ src/com/squareheads/disablegalaxygearmanagernotification/DisableGalaxyGearManagerNotification.java
@@ -2,7 +2,6 @@ package com.squareheads.disablegalaxygearmanagernotification;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XposedHelpers;
 
@@ -11,20 +10,7 @@ public class DisableGalaxyGearManagerNotification implements IXposedHookLoadPack
 
 	@Override
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-		if (lpparam.packageName.equals("com.samsung.android.app.watchmanager")){
-			
-			try {
-				XposedHelpers.findAndHookMethod(
-						"com.samsung.android.app.watchmanager.service.BManagerConnectionService",
-						lpparam.classLoader,
-						"setNotification",
-						XC_MethodReplacement.DO_NOTHING
-						);
-			} catch (Exception e) {
-				//Do nothing.
-			}
-		}
-		else if (lpparam.packageName.equals("com.samsung.android.gear1plugin")){
+		if (lpparam.packageName.equals("com.samsung.android.gear1plugin")){
 			try {
 				XposedHelpers.findAndHookMethod(
 						"com.samsung.android.gear1plugin.service.BManagerConnectionService",
@@ -36,12 +22,6 @@ public class DisableGalaxyGearManagerNotification implements IXposedHookLoadPack
 				//Do nothing
 			}
 		}
-		return;
-		
-		
-		
-		
-		
 	}
 }
 
